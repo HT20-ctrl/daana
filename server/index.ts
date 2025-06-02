aee2ceba8ef0209def8038e41202933e69cd0fa4
HT20-ctrl <hartfordtech2020@gmail.com>
2025-06-02 23:04:57 +0300

    Add dotenv configuration

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./simplified-routes";
 import { setupVite, serveStatic, log } from "./vite";
